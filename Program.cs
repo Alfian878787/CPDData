c690ed84a95975d1bfee620071deacf2ca008482
amalwayscharan <charanforms@gmail.com>
2015-04-26 22:08:47 -0500

    added logic

--- Program.cs
+++ Program.cs
@@ -10,11 +10,14 @@ namespace CPDData1
 {
     class Program
     {
+        public static List<DateTime> StartdateList = new List<DateTime>();
+        public static List<DateTime> EnddateList = new List<DateTime>();
+        public static List<double> averagesList=new List<double>();
         static void Main(string[] args)
         {
             DataTable dt = readFile();
             isValidData(dt);
-
+            calculateAverages(dt);
         }
 
         public static DataTable readFile()
@@ -82,9 +85,75 @@ namespace CPDData1
                     return false ;
                 }                  
             }
-            Console.WriteLine("Read Successfull");
+            Console.WriteLine("Read Successful");
             Console.ReadKey();
             return true;
         }
+
+        //public static void calculateAverages(DataTable dt)
+        //{
+        //    DateTime StartDay, EndDay;
+        //    foreach(DataRow dr in dt.Rows)
+        //    {
+        //        //GEt the first day and last day
+        //        StartDay = Convert.ToDateTime(dr[0].ToString());
+        //        EndDay = Convert.ToDateTime(dr[1].ToString());
+
+        //        //Get the total number of days
+        //        double TotalDays = Math.Ceiling((EndDay.Date - StartDay.Date).TotalDays+1);
+
+        //        //Clculate the average CPD for the given period
+        //        double AvgCPD = Convert.ToDouble(dr[2])/ TotalDays;
+
+        //        //Find the number of days in the month of the Startday and calculate averge
+        //        double firstmonthAvg = AvgCPD * DateTime.DaysInMonth(StartDay.Year, StartDay.Month);
+        //        averagesList.Add(firstmonthAvg);
+        //        dateList.Add(new DateTime(StartDay.Year, StartDay.Month,1));
+                
+
+        //        //Calculate the months for the months between startDay and end day
+        //        double remaningMonths = EndDay.Month - StartDay.Month;
+        //        remaningMonths = remaningMonths < 0 ? 12 + remaningMonths : remaningMonths;
+        //        if(remaningMonths>1)
+        //        {
+        //            for(int i=1;i<remaningMonths;i++)
+        //            {
+        //                DateTime tempDay = StartDay.AddMonths(i);
+        //                dateList.Add(new DateTime(tempDay.Year,tempDay.Month,1));
+        //                averagesList.Add(AvgCPD*DateTime.DaysInMonth(tempDay.Year,tempDay.Month));
+        //            }
+        //        }
+
+        //        //Find the number of days in the month of the Endday and calculate averge
+        //        double lastMonthAvg = AvgCPD * DateTime.DaysInMonth(EndDay.Year, EndDay.Month); //The number of days should be inclusive
+        //        dateList.Add(new DateTime(EndDay.Year, EndDay.Month, 1));
+        //        averagesList.Add(lastMonthAvg);
+        //    }
+        //}
+    
+    public static void calculateAverages(DataTable dt)
+    {
+        DateTime StartDay, EndDay;
+            foreach(DataRow dr in dt.Rows)
+            {
+                //GEt the first day and last day
+                StartDay = Convert.ToDateTime(dr[0].ToString());
+                EndDay = Convert.ToDateTime(dr[1].ToString());
+
+                //Get the total number of days
+                double TotalDays = Math.Ceiling((EndDay.Date - StartDay.Date).TotalDays+1);
+
+                //Calculate the average CPD for the given period
+                double AvgCPD = Convert.ToDouble(dr[2])/ TotalDays;
+
+                //Add the average CPD, first month and last month values to the list
+                averagesList.Add(AvgCPD);
+                StartdateList.Add(new DateTime(StartDay.Year, StartDay.Month, 1));
+                EnddateList.Add(new DateTime(EndDay.Year, EndDay.Month, 1));
+
+
+            }
+    }
+    
     }
 }
